73db183d9a88e1a65936b3c8075c8a8a18066e14
AlvaroMQ <alvaro.mamani.qp@gmail.com>
2025-01-11 22:15:33 -0400

    feat: add env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { CoreModule } from './core/core.module';
+import { ApplicationModule } from './application/application.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CoreModule],
+  imports: [CoreModule, ApplicationModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [],
 })
