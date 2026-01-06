bfb017d2a80247f37c7deb87822f24abfcc14ec1
AlvaroMQ <alvaro.mamani.qp@gmail.com>
2026-01-05 23:08:02 -0400

    feat: add upload image api

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigurationModule } from './config/configuration.module';
+import { UploadsController } from './upload/upload.controller';
 
 @Module({
   imports: [ConfigurationModule],
   exports: [],
+  controllers: [UploadsController]
 })
 export class CoreModule {}
---
+++ src/core/upload/upload.controller.ts
@@ -0,0 +1,58 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+  BadRequestException,
+  Get,
+  Param,
+  Res,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname, join } from 'path';
+import { Response } from 'express';
+
+@Controller('uploads')
+export class UploadsController {
+  @Post('image')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads', // Carpeta donde se guardan las imágenes
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const extension = extname(file.originalname);
+          const filename = `${file.fieldname}-${uniqueSuffix}${extension}`;
+          callback(null, filename);
+        },
+      }),
+      fileFilter: (req, file, callback) => {
+        console.log(file.mimetype);
+        if (!file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+          return callback(
+            new BadRequestException('Only image files are allowed!'),
+            false,
+          );
+        }
+        callback(null, true);
+      },
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('File is required!');
+    }
+    return {
+      message: 'File uploaded successfully',
+      file: file.filename,
+      path: `/uploads/image/${file.filename}`, // Ruta para acceder a la imagen
+    };
+  }
+
+  @Get('image/:filename')
+  getFile(@Param('filename') filename: string, @Res() res: Response) {
+    return res.sendFile(join(__dirname, '../../../../uploads', filename));
+  }
+}
